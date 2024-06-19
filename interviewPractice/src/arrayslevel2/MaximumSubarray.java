52a4bfec0a056de664c0f1ff242a3e41e5f8f2c9
gpl31.msh@gmail.com <gpl31.msh@gmail.com>
2024-06-19 18:50:41 +0530

    interview questions 19/06

---
+++ interviewPractice/src/arrayslevel2/MaximumSubarray.java
@@ -0,0 +1,32 @@
+package interviewPractice.src.arrayslevel2;
+
+public class MaximumSubarray
+{
+    static int  maxSum(int arr[], int k)
+    {
+        int currSum =0;
+        int n = arr.length;
+        int result = 0;
+
+        for(int i =0;i<k;i++)
+        {
+            currSum+=arr[i];
+        }
+        result = currSum;
+
+        for(int i=k;i<n;i++)
+        {
+            currSum +=(arr[i]-arr[i-k]);
+           result=Math.max(result,currSum);
+        }
+
+        return result;
+    }
+
+    public static void main(String args[])
+    {
+        int[] arr = {1,8,30,-5,20,7};
+        int k = 4;
+        System.out.println(maxSum(arr,k));
+    }
+}
